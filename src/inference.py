959e6e92d44e6434dfec0a02271a14fb663fdc77
Jegatheeswarir <srijega2005@gmail.com>
2026-05-25 20:34:00 +0530

    Fixed GPU issue

--- src/inference.py
+++ src/inference.py
@@ -1,14 +1,22 @@
 from ultralytics import YOLO
 from pathlib import Path
-# import torch
-# from PIL import Image
-# import numpy
+import torch
 from src.config import load_config
 
+
 class YOLOv11Inference:
-    def __init__(self, model_name, device='cuda'):
+    def __init__(self, model_name, device=None):
+
+        # Auto detect GPU or CPU
+        self.device = (
+            device if device
+            else "cuda" if torch.cuda.is_available()
+            else "cpu"
+        )
+
+        print(f"Using device: {self.device}")
+
         self.model = YOLO(model_name)
-        self.device = device
         self.model.to(self.device)
 
         # loading config from default.yaml
@@ -36,10 +44,10 @@ class YOLOv11Inference:
                 bbox = box.xyxy[0].tolist()
 
                 detection.append({
-                    'class' : cls,
-                    'confidence' : conf,
-                    'bbox' : bbox,
-                    'count' : 1
+                    'class': cls,
+                    'confidence': conf,
+                    'bbox': bbox,
+                    'count': 1
                 })
 
                 class_counts[cls] = class_counts.get(cls, 0) + 1
@@ -48,13 +56,12 @@ class YOLOv11Inference:
             det['count'] = class_counts[det['class']]
 
         return {
-            'image_path' : str(image_path),
-            'detections' : detection,
-            'total_objects' : len(detection),
-            'unique_class' : list(class_counts.keys()), # [0, 1, 2]
-            'class_counts' : class_counts # {0 : 3, 1 : 10, 2, : 1}
+            'image_path': str(image_path),
+            'detections': detection,
+            'total_objects': len(detection),
+            'unique_class': list(class_counts.keys()),
+            'class_counts': class_counts
         }
-
 
     def process_directory(self, directory):
         metadata = []
@@ -71,5 +78,5 @@ class YOLOv11Inference:
             except Exception as e:
                 print(f"Error processing {img_path}: {str(e)}")
                 continue
-        # print(metadata)
+
         return metadata
